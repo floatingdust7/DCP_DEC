3ba738e1b3adc8909af38ffd6c6ce3852a8ca2f1
floatingdust7 <371887257@qq.com>
2024-06-08 21:14:43 +0800

    注释

--- layers.py
+++ layers.py
@@ -4,6 +4,11 @@ from torch.nn.modules.module import Module
 from torch.nn.parameter import Parameter
 import math
 
+'''
+这个 GraphConvolution 类实现了图卷积网络的基本构件，它可以捕捉图中节点的局部连接模式，并用于节点分类等任务。
+权重矩阵 self.weight 在训练过程中会被优化，以最小化模型的损失函数。
+通过 forward 方法，每个节点的输出特征是通过其自身特征和邻居特征的聚合来计算的，这是图卷积网络的核心思想。
+'''
 
 class GraphConvolution(Module):
     """
@@ -14,21 +19,36 @@ class GraphConvolution(Module):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        #Parameter 是 PyTorch 中的一个类，它将一个张量包装成一个模型的参数，这样 PyTorch 就能自动地将其纳入到梯度计算和优化过程中。
+        # 这意味着在反向传播时，这些权重会根据损失函数计算得到的梯度来更新。
         self.weight = Parameter(torch.FloatTensor(in_features, out_features))
         self.reset_parameters()
 
+
     def reset_parameters(self):
+        '''
+          这里计算了一个用于权重初始化的标准差 stdv。它基于权重矩阵的第二维度（self.weight.size(1)），即输出特征的数量。
+          这是为了确保权重的方差在每一层保持一致，这是基于Xavier初始化（也称为Glorot初始化）的思想。
+          '''
         stdv = 1. / math.sqrt(self.weight.size(1))
+        '''
+        使用计算出的标准差，权重数据被初始化为一个均匀分布，范围在 -stdv 到 stdv 之间。
+        这行代码直接对 self.weight 属性的 data 部分进行操作，以避免修改 Parameter 对象本身。
+        '''
         self.weight.data.uniform_(-stdv, stdv)
         # torch.nn.init.xavier_uniform_(self.weight)
 
     def forward(self, x, adj):
+        #这行代码计算节点特征与权重矩阵 self.weight 的矩阵乘法。
         support = torch.mm(x, self.weight)
+        # 这行代码使用稀疏矩阵乘法 torch.spmm 来计算邻接矩阵 adj 和变换后的特征矩阵 support 的乘积。
         output = torch.spmm(adj, support)
         # if active:
         #     output = F.relu(output)
         return output
 
+    #当 __repr__ 方法被调用时，它会返回一个格式化的字符串，显示类的名称以及输入和输出特征的数量。
+    # 这在调试神经网络模型时特别有用，因为它可以清晰地显示层的输入和输出维度。
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
                + str(self.in_features) + ' -> ' \
--- models.py
+++ models.py
@@ -16,26 +16,37 @@ class AE(nn.Module):
     def __init__(self, n_enc_1, n_dec_1,
                  n_input, n_z):
         super(AE, self).__init__()
+        #创建第一个编码器层，是一个线性层，输入维度为n_input，输出维度为n_enc_1
         self.enc_1 = Linear(n_input, n_enc_1)
         # self.enc_2 = Linear(n_enc_1, n_enc_2)
+        #创建一个线性层，将编码器的输出映射到潜在表示空间，输入维度为n_enc_1，输出维度为n_z。
         self.z_layer = Linear(n_enc_1, n_z)
 
+        #创建第一个解码器层，是一个线性层，输入维度为n_z，输出维度为n_dec_1。
         self.dec_1 = Linear(n_z, n_dec_1)
         # self.dec_2 = Linear(n_dec_1, n_dec_2)
+        #创建最后一个解码器层，将解码器的输出映射回原始输入空间，输入维度为n_dec_1，输出维度为n_input。
         self.x_bar_layer = Linear(n_dec_1, n_input)
 
     def forward(self, x):
+        #输入数据x通过self.enc_1线性层进行变换，然后使用F.relu函数（PyTorch中的ReLU激活函数）进行非线性激活。
         enc_h1 = F.relu(self.enc_1(x))
         # enc_h2 = F.relu(self.enc_2(enc_h1))
+        #编码器的输出enc_h1通过self.z_layer线性层变换，得到潜在表示z。
         z = self.z_layer(enc_h1)
 
+        #解码器的第一层操作，将潜在表示z通过self.dec_1线性层变换，并使用ReLU激活函数。
         dec_h1 = F.relu(self.dec_1(z))
         # dec_h2 = F.relu(self.dec_2(dec_h1))
+        #解码器的输出dec_h1通过self.x_bar_layer线性层变换，得到重构的输入x_bar。
         x_bar = self.x_bar_layer(dec_h1)
 
         return x_bar, z
 
 
+'''
+为了保持原始网络结构，并使学习到的表示更具区分性，引入了一个解码器。该解码器通过节点潜在表示的内积来重建节点间的关系
+'''
 class InnerProductDecoder(nn.Module):
     """Decoder for using inner product for prediction."""
     def __init__(self, act=torch.sigmoid):
@@ -45,6 +56,10 @@ class InnerProductDecoder(nn.Module):
 
     def forward(self, z):
         # z = F.dropout(z, self.dropout, training=self.training)
+        '''
+        计算输入 z 和其转置 z.t() 之间的矩阵乘法，得到一个内积矩阵。这个内积矩阵的每个元素 [i][j] 表示 z[i] 和 z[j] 之间的点积。
+        然后，使用类的属性 self.act（默认为 Sigmoid 函数）对这个内积矩阵进行激活，得到一个介于 0 和 1 之间的邻接矩阵 adj。
+        '''
         adj = self.act(torch.mm(z, z.t()))
         return adj
 
@@ -52,9 +67,14 @@ class InnerProductDecoder(nn.Module):
 class GAE(nn.Module):
     def __init__(self, input_feat_dim, hidden_dim1, n_z):
         super(GAE, self).__init__()
+        #创建第一个图卷积层 gc1，用作自编码器的编码器部分的第一层。它将输入特征从 input_feat_dim 维度映射到 hidden_dim1 维度。
         self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1)
+        #创建第二个图卷积层 gz，用作编码器的第二层，将 hidden_dim1 维度的特征进一步编码到 n_z 维度的潜在表示空间。
         self.gz = GraphConvolution(hidden_dim1, n_z)
-
+        '''
+        self.dc 使用恒等激活函数（即 lambda x: x），这表明在给定的 GAE 类构造函数中，解码器的输出直接使用内积的结果。 
+        本来InnerProductDecoder就有sigmoid函数。
+        '''
         self.dc = InnerProductDecoder(act=lambda x: x)
 
     def forward(self, x, adj):
